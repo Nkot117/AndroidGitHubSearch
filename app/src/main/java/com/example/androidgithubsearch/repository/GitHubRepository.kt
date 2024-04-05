09d5ec43780fe98073b3ec60f1cf424fe870e421
Nkot117 <kumakura117@gmail.com>
2024-04-05 17:19:06 +0900

    リポジトリ検索APIの実装と追加

--- app/src/main/java/com/example/androidgithubsearch/repository/GitHubRepository.kt
+++ app/src/main/java/com/example/androidgithubsearch/repository/GitHubRepository.kt
@@ -1,11 +1,16 @@
 package com.example.androidgithubsearch.repository
 
 import com.example.androidgithubsearch.api.GitHubApiService
+import com.example.androidgithubsearch.model.GitHubSearchRepositoryResponse
 import com.example.androidgithubsearch.model.GitHubUserRepositoryResponse
 import javax.inject.Inject
 
 class GitHubRepository @Inject constructor(private val apiService: GitHubApiService) {
     suspend fun getRepositories(username: String): List<GitHubUserRepositoryResponse> {
-        return apiService.getRepositories(username)
+        return apiService.getUserRepositories(username)
+    }
+
+    suspend fun searchRepositories(query: String): GitHubSearchRepositoryResponse {
+        return apiService.searchRepositories(query)
     }
 }
