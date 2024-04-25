2384babbde08a898d6654d5112107cd352ab4617
Nkot117 <kumakura117@gmail.com>
2024-04-25 22:12:33 +0900

    フッタータブのタブ遷移を追加

--- app/src/main/java/com/example/androidgithubsearch/ui/MainActivity.kt
+++ app/src/main/java/com/example/androidgithubsearch/ui/MainActivity.kt
@@ -2,11 +2,26 @@ package com.example.androidgithubsearch.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.example.androidgithubsearch.R
+import com.example.androidgithubsearch.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+    private var _binding: ActivityMainBinding? = null
+    private val binding get() = _binding!!
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setUpBottomNavigation()
+    }
+    
+    private fun setUpBottomNavigation() {
+        val bottomNavigation: BottomNavigationView = binding.bottomNavigation
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        bottomNavigation.setupWithNavController(navController)
     }
 }
