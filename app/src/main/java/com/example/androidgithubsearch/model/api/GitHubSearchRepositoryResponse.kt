4604d02692e89f4159b0247ed65ff37d36fe4b82
Nkot117 <kumakura117@gmail.com>
2024-04-27 11:49:33 +0900

    パッケージ構成の変更

--- app/src/main/java/com/example/androidgithubsearch/model/api/GitHubSearchRepositoryResponse.kt
+++
@@ -1,9 +0,0 @@
-package com.example.androidgithubsearch.model.api
-
-import com.squareup.moshi.Json
-
-data class GitHubSearchRepositoryResponse(
-    @field:Json(name = "total_count")
-    val total_count: Int,
-    val items: List<GitHubUserRepositoryResponse>
-)
